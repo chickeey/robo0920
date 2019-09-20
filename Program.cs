3fa57fd4cd2398ee1538d7a851fb10e88b68776f
Simon Gergő <simon.geri@hotmail.com>
2019-09-20 12:25:10 +0200

    Most már csillagos c:

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace robo0920
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("Szia, hogy hívnak?");
@@ -17,12 +18,19 @@ namespace robo0920
             var valasz = Console.ReadLine();
             if (valasz.ToLower() == "igen")
             {
-                Console.WriteLine("akkor mi nagyon jó barátok leszünk :)");
+                Console.WriteLine("Akkor mi nagyon jó barátok leszünk :)");
             }
             else
             {
                 Console.WriteLine(":(");
                 Console.WriteLine("Szevasz :c");
+            }
+            Console.WriteLine("Tessék 100 csillag c: <3");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.SetCursorPosition(rnd.Next(Console.WindowWidth), rnd.Next(Console.WindowHeight));
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+                Console.Write("*");
             }
             Console.ReadKey();
         }
