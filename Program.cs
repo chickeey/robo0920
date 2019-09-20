3a0a26205c03a7e50024d2798481b26f938aed9c
simon.gergo@vereb.dc <simon.gergo@vereb.dc>
2019-09-20 12:00:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robo0920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia, hogy hívnak?");
+            var nev = Console.ReadLine();
+            Console.WriteLine($"Milyen szép név az hogy {nev}");
+            Console.WriteLine("Szeretsz programozni?");
+            var valasz = Console.ReadLine();
+            if (valasz.ToLower() == "igen")
+            {
+                Console.WriteLine("akkor mi nagyon jó barátok leszünk :)");
+            }
+            else
+            {
+                Console.WriteLine(":(");
+                Console.WriteLine("Szevasz :c");
+            }
+            Console.ReadKey();
+        }
+    }
+}
